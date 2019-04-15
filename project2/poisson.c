725cb368e4247376c62b46d804d616ed507d2c52
mariarrstud <47381143+mariarrstud@users.noreply.github.com>
2019-04-15 17:47:49 +0200

    Update poisson.c

--- project2/poisson.c
+++ project2/poisson.c
@@ -13,7 +13,6 @@ typedef int bool;
 
 real *mk_1D_array(size_t n, bool zero);
 real **mk_2D_array(size_t n1, size_t n2, bool zero);
-void transpose(real **bt, real **b, size_t m);
 real rhs(real x, real y);
 
 void fst_(real *v, int *n, real *w, int *nn);
